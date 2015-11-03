a29d33ab5cc857211280a315c6067322bfd5e900
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-02 19:48:11 -0500

    angler: setup omni makefiles

--- omni_angler.mk
+++ omni_angler.mk
@@ -13,33 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit from the common Open Source product configuration
+# Inherit AOSP base telephony config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    charger
-
+# TWRP
 PRODUCT_COPY_FILES += \
-    device/huawei/angler/kernel:kernel
+    device/huawei/angler/twrp.fstab:recovery/root/etc/twrp.fstab
 
+# Override product naming for Omni
 PRODUCT_NAME := omni_angler
+PRODUCT_BRAND := google
 PRODUCT_DEVICE := angler
-PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
-
-# Kernel inline build
-#TARGET_KERNEL_CONFIG := angler_defconfig
-#TARGET_VARIANT_CONFIG := angler_defconfig
-#TARGET_SELINUX_CONFIG := angler_defconfig
+PRODUCT_RESTRICT_VENDOR_FILES := false

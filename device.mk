46deea7be08e7cf228444bc76360e04dcfdbf088
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-04-17 22:49:02 -0700

    Use new 3gb dalvik heap and hwui Needs the corresponding files added to frameworks_native
    Change-Id: I6f57295df3879217c5f779210c4097014a6f3ea3
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- device.mk
+++ device.mk
@@ -303,18 +303,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.force_eri_from_xml=true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
+# configure the HWUI memory limits
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 # VR HAL
 PRODUCT_PACKAGES += \
@@ -486,7 +476,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
 
 # setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 
 # drmservice prop
 PRODUCT_PROPERTY_OVERRIDES += \

4f6b9c360dda7d75b347a6479daf728c5ed748e7
Jarrod Worlitz <jarrod.worlitz@gmail.com>
2017-05-18 23:29:41 -0700

    angler: makefiles: add AOKP makefile -builds off the same tree

---
+++ aokp.mk
@@ -0,0 +1,29 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+# Copy over our ramdisk files
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/ramdisk/init.flash.rc:root/init.flash.rc \
+    vendor/aokp/prebuilt/ramdisk/init.profiles.rc:root/init.profiles.rc \
+    vendor/aokp/prebuilt/ramdisk/init.profiles.sh:root/init.profiles.sh \
+    vendor/aokp/prebuilt/ramdisk/init.special_power.sh:root/init.special_power.sh \
+    vendor/aokp/prebuilt/ramdisk/msm_irqbalance.conf:root/msm_irqbalance.conf
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aokp_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := Huawei
+TARGET_VENDOR := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47O/3852959:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47O 3852959 release-keys"

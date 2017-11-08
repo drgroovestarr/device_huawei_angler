nathanchance/android_device_huawei_angler

Start compiling GApps inline
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

device.mk
@@ -515,3 +515,9 @@ endif
 # b/29995499
 $(call add-product-sanitizer-module-config,cameraserver,never)
 $(call add-product-sanitizer-module-config,mm-qcamera-daemon,never)
+
+# Open GApps
+ifneq ($(HAS_GAPPS),false)
+$(call inherit-product-if-exists, device/huawei/angler/opengapps.mk)
+$(call inherit-product-if-exists, vendor/google/build/opengapps-packages.mk)
+endif

drgroovestarr/device_huawei_angler

device.mk
@@ -593,3 +593,9 @@ $(call add-product-sanitizer-module-config,mm-qcamera-daemon,never)
 # b/36201281
 $(call add-product-sanitizer-module-config,thermal-engine,never)
 $(call add-product-sanitizer-module-config,qmuxd,never)
+
+# Open GApps
+ifneq ($(HAS_GAPPS),false)
+$(call inherit-product-if-exists, device/huawei/angler/opengapps.mk)
+$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
+endif